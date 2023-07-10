2821b3f26dbd496ff24e341223e8d6ea549c3e58
parthivvekariya <137589220+parthivvekariya@users.noreply.github.com>
2023-07-10 08:05:21 +0530

    Add files via upload

---
+++ Module – 1/2Calculator.dart
@@ -0,0 +1,17 @@
+void main ()
+{
+  int a =10;
+  int b =20;
+
+  int add =a+b;
+  int sub =a-b;
+  int mul =a*b;
+  double div =(a/b) ;
+  int mod =a%b;
+
+  print(add);
+  print(sub);
+  print(mul);
+  print(div);
+  print(mod);
+}
